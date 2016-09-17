5a9843c95bd4fa5e9b928c56eca3716e18c31c59
Amadeusz Juskowiak <amadeusz@amanointeractive.com>
2016-09-17 13:36:42 +0200

    magic chart generator

--- generate_charts.py
+++ generate_charts.py
@@ -5,6 +5,8 @@ import argparse
 from os.path import basename
 import os
 
+def avg(a):
+    return float(sum(a))/len(a)
 
 class Dataset:
     def __init__(self, log):
@@ -57,6 +59,121 @@ class Dataset:
 class PlotGenerator:
     def __init__(self, datasets):
         self.datasets = datasets
+
+    def multiple_instance_aggregated_key(self, key, aggregate, label=None,
+                                         states=('crawling',),
+                                         title=True, legend=True):
+        figure, plt = matplotlib.pyplot.subplots()
+
+        plt.set_axisbelow(True)
+        figure.set_size_inches((18, 6), forward=True)
+        plt.grid(b=True, which='major', color='lightgray', linestyle='-')
+        plt.grid(b=True, which='minor', color='whitesmoke', linestyle='-')
+        plt.minorticks_on()
+
+        max_epoch = 0
+
+        for dataset in self.datasets:
+            local_max_epoch = 0
+            data = {}
+
+            for entry in dataset.plasmodia['*']:
+                if entry['epoch'] > local_max_epoch:
+                    local_max_epoch = entry['epoch']
+
+                if entry['epoch'] not in data:
+                    data[entry['epoch']] = []
+
+            if local_max_epoch > max_epoch:
+                max_epoch = local_max_epoch
+
+            for name in dataset.plasmodia:
+                if isinstance(name, int):
+                    for entry in dataset.plasmodia[name]:
+                        if entry['state'] in states:
+                            data[entry['epoch']].append(entry[key])
+
+            aggregated_data = []
+
+            for epoch, values in data.iteritems():
+                try:
+                    aggregated_data.append(aggregate(values))
+                except:
+                    aggregated_data.append(0)
+
+            plt.plot(aggregated_data, label=dataset.name)
+
+        if label is None:
+            label = key
+
+        plt.set_ylabel(label)
+        plt.set_xlabel('epoch')
+        plt.set_xlim(right=max_epoch)
+        x1, x2, y1, y2 = plt.axis()
+
+        if y1 < 10 and y2 > 1.5:
+            plt.axis((x1, x2, 0, y2*1.01))
+
+        if legend:
+            plt.legend()
+
+        if title is True:
+            plt.set_title("%s aggregated %s - %s" %
+                            (key, aggregate.__name__, dataset.name))
+        elif isinstance(title, basestring):
+            plt.set_title(title)
+
+
+        return [figure]
+
+
+    def multiple_instance_single_key(self, key, label=None, title=True,
+                                     legend=True):
+        figure, plt = matplotlib.pyplot.subplots()
+
+        plt.set_axisbelow(True)
+        figure.set_size_inches((18, 6), forward=True)
+        plt.grid(b=True, which='major', color='lightgray', linestyle='-')
+        plt.grid(b=True, which='minor', color='whitesmoke', linestyle='-')
+        plt.minorticks_on()
+
+        max_epoch = 0
+
+        for dataset in self.datasets:
+            data = []
+
+            for entry in dataset.plasmodia['*']:
+                if entry['epoch'] > max_epoch:
+                    max_epoch = entry['epoch']
+
+                if key in entry:
+                    data.append(entry[key])
+
+            plt.plot(data, label=dataset.name)
+
+        if label is None:
+            label = key
+
+        plt.set_ylabel(label)
+        plt.set_xlabel('epoch')
+        plt.set_xlim(right=max_epoch)
+        x1, x2, y1, y2 = plt.axis()
+
+        if y1 < 10 and y2 > 1.5:
+            plt.axis((x1, x2, 0, y2*1.01))
+
+        if legend:
+            plt.legend()
+
+        if title is True:
+            plt.set_title("%s - %s" %
+                            (key, dataset.name))
+        elif isinstance(title, basestring):
+            plt.set_title(title)
+
+
+        return [figure]
+
 
     def single_instance_plasmodia_state(self):
         plots = []
@@ -122,11 +239,10 @@ class PlotGenerator:
             figure, plt = matplotlib.pyplot.subplots()
             plt.set_axisbelow(True)
 
-            figure.set_size_inches((18,6), forward=True)
+            figure.set_size_inches((18, 6), forward=True)
             plt.grid(b=True, which='major', color='lightgray', linestyle='-')
             plt.grid(b=True, which='minor', color='whitesmoke', linestyle='-')
             plt.minorticks_on()
-
 
             names = filter(lambda x: isinstance(x, int),
                            dataset.plasmodia.keys())
@@ -167,6 +283,9 @@ class PlotGenerator:
 
 
 if __name__ == '__main__':
+    matplotlib.pyplot.rc('text', usetex=True)
+    matplotlib.pyplot.rc('font', family='serif')
+
     parser = argparse.ArgumentParser(description="Charts generator")
 
     parser.add_argument('-l', '--log', type=argparse.FileType('r'),
@@ -184,36 +303,35 @@ if __name__ == '__main__':
 
     f = []
 
+    f += generator.multiple_instance_single_key('total_food_eaten_count',
+                                                label='number of visited solutions')
+
+    f += generator.multiple_instance_single_key('memory',
+                                                label='total memory usage')
+
+    f += generator.multiple_instance_single_key('time',
+                                                label='execution time')
+
     f += generator.single_instance_plasmodia_state()
 
-    f += generator.single_instance_multiple_plasmodia('food',
-                                                      states=('new','crawled'),
-                                                      label='plasmodium energy')
+    detailed = {'food': (('new', 'crawled'), 'plasmodium energy'),
+                'size': (('new', 'crawled', 'dead'), 'number of occupied solutions'),
+                'total_explored': (('new', 'crawled'), 'total number of explored solutions'),
+                'total_crawled': (('new', 'crawled'), 'total number of crawled solutions'),
+                'frontier': (('crawled', 'merged'), 'number of solutions in frontier'),
+                'frontier_best_cost': (('crawled',), 'cost of best solution in frontier'),
+                'frontier_worst_cost': (('crawled',), 'cost of worst solution in frontier')}
 
-    f += generator.single_instance_multiple_plasmodia('size',
-                                                      states=('new','crawled',
-                                                              'dead'),
-                                                      label='number of occupied solutions')
+    aggregates = (min, max, sum, avg)
 
-    f += generator.single_instance_multiple_plasmodia('total_explored',
-                                                      states=('new', 'crawled'),
-                                                      label='number of explored solutions')
+    for key, conf in detailed.iteritems():
+        f += generator.single_instance_multiple_plasmodia(key, states=conf[0],
+                                                          label=conf[1])
 
-    f += generator.single_instance_multiple_plasmodia('total_crawled',
-                                                      states=('new', 'crawled'),
-                                                      label='number of crawled solutions')
-
-    f += generator.single_instance_multiple_plasmodia('frontier',
-                                                      states=('crawled','merged'),
-                                                      label='number of solutions in frontier')
-
-    f += generator.single_instance_multiple_plasmodia('frontier_best_cost',
-                                                      states=('crawled',),
-                                                      label='best cost in frontier')
-
-    f += generator.single_instance_multiple_plasmodia('frontier_worst_cost',
-                                                      states=('crawled',),
-                                                      label='worst cost in frontier')
+        for agg in aggregates:
+            f += generator.multiple_instance_aggregated_key(key, agg,
+                                                            states=conf[0],
+                                                            label=conf[1])
 
     for plot in f:
         filename = plot.gca().title.get_text()
@@ -223,9 +341,9 @@ if __name__ == '__main__':
                      bbox_inches='tight')
 
         plot.gca().set_xlim(0, 0.2 * plot.gca().get_xlim()[1])
-        plot.savefig("%s/zoom_%s.pdf" % (args.out, filename),
+        plot.savefig("%s/%s.zoomed.pdf" % (args.out, filename),
                      bbox_inches='tight')
 
         plot.gca().set_xlim(0, 0.25 * plot.gca().get_xlim()[1])
-        plot.savefig("%s/superzoom_%s.pdf" % (args.out, filename),
+        plot.savefig("%s/%s.zzzoomed.pdf" % (args.out, filename),
                      bbox_inches='tight')
